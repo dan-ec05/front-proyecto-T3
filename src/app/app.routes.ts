dbfa937a657ef4cc70a7c7c910a19e141082a60d
danidan2905 <danielenrriquecamacho12@gmail.com>
2024-04-28 22:14:26 -0400

    Inicio de sesion listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/container/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./auth/container/login/login.component').then(component => {
+            return component.LoginComponent;
+        }),
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }
+];
