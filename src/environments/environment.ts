b7cca5f9bef93de0d19da04ced5b9115c7bf70bc
danidan2905 <danielenrriquecamacho12@gmail.com>
2024-12-01 17:46:57 -0400

    Corregido env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     name: "DEV",
-    API: "http://localhost:3000"
+    // API: "http://localhost:3000"
     // API: "https://backend-ccvma.onrender.com",
-    // API: "https://calm-belia-daniel-ccvma-5f7b13ae.koyeb.app"
+    API: "https://calm-belia-daniel-ccvma-5f7b13ae.koyeb.app"
 };
